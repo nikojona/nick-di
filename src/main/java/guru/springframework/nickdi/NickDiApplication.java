334b71f4cc88f7a96f79b7fc087371828efa6749
nickolas.grosenia <nickolas@grosenia.co.id>
2020-10-04 15:25:56 +0700

    first time commit for Dependencies Injection

---
+++ src/main/java/guru/springframework/nickdi/NickDiApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.nickdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NickDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NickDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/nickdi/NickDiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.nickdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NickDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
