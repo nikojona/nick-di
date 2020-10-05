0ab72ed7775eef89dd05282a3ab96d3f3780cbc8
nickolas.grosenia <nickolas@grosenia.co.id>
2020-10-05 13:47:15 +0700

    guru spring framework - 39. Default Profile

--- src/main/java/guru/springframework/nickdi/services/I18NIndonesiaGreetingService.java
+++ src/main/java/guru/springframework/nickdi/services/I18NIndonesiaGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.nickdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ID")
+@Profile({"ID", "default"})
 @Service("i18nService")
 public class I18NIndonesiaGreetingService implements GreetingService {
 
